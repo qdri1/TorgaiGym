e04ec8c5b2b4225fbfbcb1faf20a9ecdac840609
Kudri <qdrea95@gmail.com>
2018-06-20 19:09:17 +0600

    Five days fragments for viewpager

---
+++ app/src/main/java/com/torgaigym/torgai/torgaigym/fragments/DayFourFragment.java
@@ -0,0 +1,30 @@
+package com.torgaigym.torgai.torgaigym.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.torgaigym.torgai.torgaigym.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DayFourFragment extends Fragment {
+
+
+    public DayFourFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_day_four, container, false);
+    }
+
+}
