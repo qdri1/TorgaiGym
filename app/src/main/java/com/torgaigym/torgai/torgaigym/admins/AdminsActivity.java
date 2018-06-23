7cd7375546d030a96a374d04782110874e601ba9
Kudri <qdrea95@gmail.com>
2018-06-23 22:37:57 +0600

    ViewPager with tabs is done

--- app/src/main/java/com/torgaigym/torgai/torgaigym/admins/AdminsActivity.java
+++ app/src/main/java/com/torgaigym/torgai/torgaigym/admins/AdminsActivity.java
@@ -3,17 +3,21 @@ package com.torgaigym.torgai.torgaigym.admins;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import com.google.firebase.FirebaseApp;
 import com.torgaigym.torgai.torgaigym.R;
+import com.torgaigym.torgai.torgaigym.adapters.PagerAdapter;
 import com.torgaigym.torgai.torgaigym.classes.Exercise;
 import com.torgaigym.torgai.torgaigym.firebase.FirebaseConsts;
 import com.torgaigym.torgai.torgaigym.firebase.FirebaseUtils;
 
-public class AdminsActivity extends AppCompatActivity implements FirebaseUtils.ExerciseListener {
+public class AdminsActivity extends AppCompatActivity implements FirebaseUtils.ExerciseListener, TabLayout.OnTabSelectedListener, View.OnClickListener {
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +26,31 @@ public class AdminsActivity extends AppCompatActivity implements FirebaseUtils.E
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.day_1)));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.day_2)));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.day_3)));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.day_4)));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.day_5)));
 
+        viewPager = findViewById(R.id.pager);
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(this);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        fab.setOnClickListener(this);
+    }
 
-//                FirebaseUtils firebaseUtils = new FirebaseUtils();
+    @Override
+    public void getResponse(Exercise exercise) {
+        System.out.println("CHECK_READ### " + exercise.getName() + " / " + exercise.getDescription());
+    }
+
+    @Override
+    public void onClick(View v) {
+        //                FirebaseUtils firebaseUtils = new FirebaseUtils();
 //                Exercise exercise = new Exercise("ExampleOne", "This is just example");
 //                firebaseUtils.writeDayExercises(FirebaseConsts.dayOne, exercise);
 //                exercise = new Exercise("ExampleTwo", "This is just example");
@@ -41,17 +62,25 @@ public class AdminsActivity extends AppCompatActivity implements FirebaseUtils.E
 //                exercise = new Exercise("ExampleFive", "This is just example");
 //                firebaseUtils.writeDayExercises(FirebaseConsts.dayFive, exercise);
 
-                FirebaseUtils firebaseUtils = new FirebaseUtils();
-                firebaseUtils.readDayExercises(FirebaseConsts.dayOne, AdminsActivity.this);
+        FirebaseUtils firebaseUtils = new FirebaseUtils();
+        firebaseUtils.readDayExercises(FirebaseConsts.dayOne, AdminsActivity.this);
 
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 
     @Override
-    public void getResponse(Exercise exercise) {
-        System.out.println("CHECK_READ### " + exercise.getName() + " / " + exercise.getDescription());
+    public void onTabSelected(TabLayout.Tab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
     }
 }
